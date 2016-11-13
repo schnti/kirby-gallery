6f19ce6bb2dae1956969a9d350b96934b8020716
schnti <timo@schnti.de>
2016-11-13 08:47:34 +0100

    Updates.

--- gallery.php
+++ gallery.php
@@ -2,20 +2,25 @@
 
 kirbytext::$tags['gallery'] = array(
     'attr' => array(
-        'text',
+        'width',
     ),
     'html' => function ($tag) {
 
-        $url = $tag->attr('gallery');
+        //        $url = $tag->attr('gallery');
 
-        $title = $tag->attr('text');
+        $width = $tag->attr('width');
+        if (empty($width)) {
+            $width = c::get('ka.gallery.width', 85);
+        }
+
+        $rel = uniqid();
 
         $images = $tag->page()->images();
 
         $html = '<section class="gallery">';
         foreach ($images->sortBy('sort', 'asc') as $image):
-            $html .= '<a class="fancybox" rel="images" href="' . $image->url() . '" title="' . $image->text()->kirbytext() . '">';
-            $html .= thumb($image, array('width' => 85, 'height' => 85, 'crop' => true, 'alt' => $image->caption()->html()));
+            $html .= '<a class="fancybox" rel="group-' . $rel . '" href="' . $image->width(900, 900)->url() . '" title="' . $image->text()->kirhtmlbytext() . '">';
+            $html .= '<img src="' . $image->crop($width, $width)->url() . '" alt="' . $image->text()->html() . '">';
             $html .= '</a>';
         endforeach;
         $html .= '</section>';
